5f5040783cf133a61c376d9e9397822ebc9e469d
yutah94 <yuta.young@gmail.com>
2020-03-31 12:53:50 -0600

    Updates

---
+++ precache-manifest.e9089298c9206739ba9649720d6217dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f66d31cf1cfb04a0ddead24a4072e67f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1327395c1cfc10d68d21",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "96d65f365cc045ce51a7",
+    "url": "/static/css/main.5e695861.chunk.css"
+  },
+  {
+    "revision": "1327395c1cfc10d68d21",
+    "url": "/static/js/2.95d3ae63.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/static/js/2.95d3ae63.chunk.js.LICENSE"
+  },
+  {
+    "revision": "96d65f365cc045ce51a7",
+    "url": "/static/js/main.81e3d4cc.chunk.js"
+  },
+  {
+    "revision": "9e19e0944bfc6625df6b",
+    "url": "/static/js/runtime-main.e0c44f74.js"
+  }
+]);
