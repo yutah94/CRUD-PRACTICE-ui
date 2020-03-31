95c1ee8c1794cd92df4dd9835beb485e0a266752
yutah94 <yuta.young@gmail.com>
2020-03-31 15:29:10 -0600

    b

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'
+// import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 import TodoList from './components/TodoList';
 import AppNavbar from './components/AppNavbar';
